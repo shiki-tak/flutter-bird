56c5cf38d07c9c962b7571aaaf8fd9408b701e56
shiki-tak <shiki.tak@gmail.com>
2024-07-08 20:18:42 +0900

    ci: deploy contract using truffle and docker

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:16
+
+WORKDIR /app/flutter_bird_skins
+
+RUN apt-get update && apt-get install -y \
+    python3 \
+    make \
+    g++ \
+    libcairo2-dev \
+    libpango1.0-dev \
+    libjpeg-dev \
+    libgif-dev \
+    librsvg2-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN npm install -g node-gyp
+RUN npm install -g truffle
+
+COPY ./flutter_bird_skins/package*.json ./
+RUN npm install
+
+COPY ./flutter_bird_skins .
+
+CMD ["sh", "-c", "truffle develop"]
--- flutter_bird_skins/truffle-config.js
+++ flutter_bird_skins/truffle-config.js
@@ -1,58 +1,26 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation, and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * https://trufflesuite.com/docs/truffle/reference/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
-
 require('dotenv').config();
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const {API_URL, MNEMONIC, ETHERSCAN_API_KEY} = process.env;
+const HDWalletProvider = require("truffle-hdwallet-provider-klaytn");
 
 module.exports = {
-    /**
-     * Networks define how you connect to your ethereum client and let you set the
-     * defaults web3 uses to send transactions. If you don't specify one truffle
-     * will spin up a development blockchain for you on port 9545 when you
-     * run `develop` or `test`. You can ask a truffle command to use a specific
-     * network from the command line, e.g
-     *
-     * $ truffle test --network <network-name>
-     */
-
     networks: {
-        // Local Ganache Dummy-Blockchain
         development: {
             host: "127.0.0.1",
             port: 7545,
             network_id: "*",
         },
 
-        // Public Goerli Testnet
-        goerli: {
+        baobab: {
             provider: () => {
-                return new HDWalletProvider(MNEMONIC, API_URL)
+              return new HDWalletProvider(process.env.PRIVATE_KEY, "https://public-en-baobab.klaytn.net");
             },
-            network_id: 5,
-            gas: 4465030,
-            gasPrice: 20000000000,
-        },
+            network_id: "1001", //Klaytn baobab testnet's network id
+            gas: "8500000",
+            gasPrice: null,
+          },
+
+
     },
 
-    // Configure your compilers
     compilers: {
         solc: {
             version: "0.8.14",
@@ -62,8 +30,4 @@ module.exports = {
     plugins: [
         'truffle-plugin-verify'
     ],
-
-    api_keys: {
-        etherscan: ETHERSCAN_API_KEY
-    }
 };

0889d27462f4fb0b5c16bca12628c16d5e0e577f
shiki-tak <shiki.tak@gmail.com>
2024-07-19 11:17:42 +0900

    temp

--- flutter_bird_app/lib/config.dart
+++ flutter_bird_app/lib/config.dart
@@ -1,5 +1,5 @@
 const chainId = 1001; // Klaytn Testnet baobab
-const flutterBirdSkinsContractAddress = '0xc67AD7E04D8042Ea40799B21248E038D5f58660D';
+const flutterBirdSkinsContractAddress = '0xde8056279F21F43725cE2226457E98F1cDFef1f3';
 const ipfsGatewayUrl = 'https://nftstorage.link/ipfs/'; //FIXME: 
 const alchemyNodeProviderUrl = 'https://eth-goerli.g.alchemy.com/v2/';
 const walletConnectBridge = 'https://bridge.walletconnect.org';
